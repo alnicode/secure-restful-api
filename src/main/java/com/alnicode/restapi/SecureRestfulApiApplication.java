de7faa7a061cfeca54ecc1522d7c0bfe8628ab2c
Alben Bustamante <albenbustamante@gmail.com>
2022-05-17 19:26:12 -0500

    Start of the project

---
+++ src/main/java/com/alnicode/restapi/SecureRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.alnicode.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureRestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureRestfulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alnicode/restapi/SecureRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alnicode.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureRestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
