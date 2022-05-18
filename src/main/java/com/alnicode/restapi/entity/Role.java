c9a14f1d77812fcdccb04679c9a091f538e3c9a5
Alben Bustamante <albenbustamante@gmail.com>
2022-05-17 22:32:08 -0500

    Create role entity

---
+++ src/main/java/com/alnicode/restapi/entity/Role.java
@@ -0,0 +1,31 @@
+package com.alnicode.restapi.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @NotNull
+    @Size(min = 2)
+    @Column(unique = true)
+    private String name;
+}
