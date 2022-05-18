e0df4209819e76ef6e6a7d0de1c1163d1b888cbe
Alben Bustamante <albenbustamante@gmail.com>
2022-05-17 20:28:44 -0500

    Create the main service templates

---
+++ src/main/java/com/alnicode/restapi/service/IDirectionService.java
@@ -0,0 +1,7 @@
+package com.alnicode.restapi.service;
+
+import com.alnicode.restapi.dto.DirectionRequest;
+import com.alnicode.restapi.dto.DirectionResponse;
+
+public interface IDirectionService extends ICrudService<DirectionRequest, DirectionResponse> {
+}
---
+++ src/main/java/com/alnicode/restapi/service/IPersonService.java
@@ -0,0 +1,7 @@
+package com.alnicode.restapi.service;
+
+import com.alnicode.restapi.dto.PersonRequest;
+import com.alnicode.restapi.dto.PersonResponse;
+
+public interface IPersonService extends ICrudService<PersonRequest, PersonResponse> {
+}
