cde127cf75179bc14502202d5bf8ed0c18d0a01d
Alben Bustamante <albenbustamante@gmail.com>
2022-05-17 19:50:16 -0500

    Create person entity

---
+++ src/main/java/com/alnicode/restapi/entity/Person.java
@@ -0,0 +1,30 @@
+package com.alnicode.restapi.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "persons")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "person_id")
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @NotNull
+    @Size(min = 3, max = 120)
+    private String name;
+}
