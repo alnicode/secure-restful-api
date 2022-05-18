6623fb9a05db96ae1356165514cf5ed7f1771516
Alben Bustamante <albenbustamante@gmail.com>
2022-05-17 20:00:00 -0500

    Define relationships between persons and directions

--- src/main/java/com/alnicode/restapi/entity/Direction.java
+++ src/main/java/com/alnicode/restapi/entity/Direction.java
@@ -1,10 +1,13 @@
 package com.alnicode.restapi.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -35,4 +38,8 @@ public class Direction {
     @Size(min = 3, max = 120)
     @Column(unique = true)
     private String address;
+
+    @OneToOne(mappedBy = "direction", fetch = FetchType.LAZY, cascade = {
+            CascadeType.PERSIST, CascadeType.MERGE })
+    private Person person;
 }
--- src/main/java/com/alnicode/restapi/entity/Person.java
+++ src/main/java/com/alnicode/restapi/entity/Person.java
@@ -1,30 +1,41 @@
 package com.alnicode.restapi.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
 @Entity
 @Table(name = "persons")
 public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "person_id")
-    @EqualsAndHashCode.Include
     private Long id;
 
     @NotNull
     @Size(min = 3, max = 120)
     private String name;
+
+    @NotNull
+    @Min(1L)
+    @Max(Long.MAX_VALUE)
+    @Column(name = "direction_id", unique = true)
+    private Long directionId;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = { CascadeType.ALL })
+    @JoinColumn(name = "direction_id", insertable = false, updatable = false)
+    private Direction direction;
 }
