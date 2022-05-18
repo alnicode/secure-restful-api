30c685f05c8c19499e7368a1a984e587a0d08774
Alben Bustamante <albenbustamante@gmail.com>
2022-05-17 22:35:29 -0500

    Establish the relationship between users and roles

--- src/main/java/com/alnicode/restapi/entity/Role.java
+++ src/main/java/com/alnicode/restapi/entity/Role.java
@@ -1,10 +1,15 @@
 package com.alnicode.restapi.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.Collections;
+import java.util.Set;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -28,4 +33,8 @@ public class Role {
     @Size(min = 2)
     @Column(unique = true)
     private String name;
+
+    @ManyToMany(mappedBy = "roles", cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+    @JsonIgnore
+    private Set<User> users = Collections.emptySet();
 }
--- src/main/java/com/alnicode/restapi/entity/User.java
+++ src/main/java/com/alnicode/restapi/entity/User.java
@@ -1,10 +1,17 @@
 package com.alnicode.restapi.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -31,4 +38,11 @@ public class User {
 
     @NotNull
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 }
