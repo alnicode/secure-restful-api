0604c5a60028bf23a49eda334376411dad686e24
Alben Bustamante <albenbustamante@gmail.com>
2022-05-17 19:54:42 -0500

    Create direction entity

---
+++ src/main/java/com/alnicode/restapi/entity/Direction.java
@@ -0,0 +1,38 @@
+package com.alnicode.restapi.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "directions")
+public class Direction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "direction_id")
+    private Long id;
+
+    @NotNull
+    @Size(min = 3, max = 100)
+    private String country;
+
+    @NotNull
+    @Size(min = 3, max = 100)
+    private String state;
+
+    @NotNull
+    @Size(min = 3, max = 100)
+    private String locality;
+
+    @NotNull
+    @Size(min = 3, max = 120)
+    @Column(unique = true)
+    private String address;
+}
