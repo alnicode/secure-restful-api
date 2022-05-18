a9090ed09b7342dd7e17252a020ded7a6f9bb95e
Alben Bustamante <albenbustamante@gmail.com>
2022-05-17 22:30:30 -0500

    Create user entity

---
+++ src/main/java/com/alnicode/restapi/entity/User.java
@@ -0,0 +1,34 @@
+package com.alnicode.restapi.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @NotNull
+    @Size(min = 5)
+    @Column(unique = true)
+    private String username;
+
+    @NotNull
+    private String password;
+}
