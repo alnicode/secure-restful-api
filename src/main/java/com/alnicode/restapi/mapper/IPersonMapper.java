641f2dd10a9ea9ee5b400b0831278c48605ed3ee
Alben Bustamante <albenbustamante@gmail.com>
2022-05-17 21:45:13 -0500

    Add new expression with getFullAddress method

--- src/main/java/com/alnicode/restapi/mapper/IPersonMapper.java
+++ src/main/java/com/alnicode/restapi/mapper/IPersonMapper.java
@@ -9,7 +9,7 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface IPersonMapper {
-    @Mapping(target = "address", ignore = true)
+    @Mapping(target = "address", expression = "java(entity.getDirection().getFullAddress())")
     PersonResponse toResponse(Person entity);
     List<PersonResponse> toResponses(List<Person> entities);
 
