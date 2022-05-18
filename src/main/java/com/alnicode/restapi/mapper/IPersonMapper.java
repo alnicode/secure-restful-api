c31d5dcc6797a723ee710aaec0326af4b615da14
Alben Bustamante <albenbustamante@gmail.com>
2022-05-17 20:19:39 -0500

    Create person entity/dto mapper

---
+++ src/main/java/com/alnicode/restapi/mapper/IPersonMapper.java
@@ -0,0 +1,19 @@
+package com.alnicode.restapi.mapper;
+
+import com.alnicode.restapi.dto.PersonRequest;
+import com.alnicode.restapi.dto.PersonResponse;
+import com.alnicode.restapi.entity.Person;
+import java.util.List;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface IPersonMapper {
+    @Mapping(target = "address", ignore = true)
+    PersonResponse toResponse(Person entity);
+    List<PersonResponse> toResponses(List<Person> entities);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "direction", ignore = true)
+    Person toEntity(PersonRequest request);
+}
